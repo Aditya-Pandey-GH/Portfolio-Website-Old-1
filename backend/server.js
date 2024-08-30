307a02091a8beda072624e369a610527091e117b
Aditya Pandey <adityapandey1609@gmail.com>
2024-08-30 18:19:52 +0530

    v2.0

--- backend/server.js
+++ backend/server.js
@@ -97,5 +97,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on port http://127.0.0.1:${port}`);
+	// console.log(`Server is running on port http://127.0.0.1:${port}`);
 });
