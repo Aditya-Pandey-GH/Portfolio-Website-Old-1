244a4aa8b3a022d7550c89c4f9067604a1b118ec
Aditya Pandey <97934487+PandeyAdi-GZ@users.noreply.github.com>
2024-09-23 08:27:23 +0530

    v3.0

--- frontend/src/Comps/Home.jsx
+++ frontend/src/Comps/Home.jsx
@@ -58,8 +58,11 @@ const Home = () => {
 								<div className="homeWelfareContent">
 									<ul className="homeWelfareSubContent" style={{ flexDirection: "column", marginLeft: "1rem" }}>
 										<li>Hello user. I am Aditya Pandey👋, and I welcome you to my website.</li>
-										<li>I am a BCA graduate, and have the knowledge of making web apps.</li>
-										<li>Explore this website to know more about me.</li>
+										<li>I am a BCA graduate, and have the knowledge of making web applications 🌐, like this website.</li>
+										<li>Other than that, I also have knowledge of various concepts like graphic designing 🖼️ and video editing 📹.</li>
+										<li>I am aspiring to be a professional game developer 🎮 in near future, to be able to lead the market and industry with my professionalism.</li>
+										<li>This website shows my skills, my works, my achievemnents 🏆, my experience, and many more things about me.</li>
+										<li>Explore this website to know more about me 😁.</li>
 									</ul>
 								</div>
 							</div>
