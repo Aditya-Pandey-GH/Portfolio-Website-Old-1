ffafdd3dc84f83f77db3405e5e20022b32e7441b
Aditya Pandey <97934487+PandeyAdi-GZ@users.noreply.github.com>
2024-09-25 01:28:46 +0530

    c

--- frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import Data from "./Data.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<>
-		<Data />
-	</>
-);
